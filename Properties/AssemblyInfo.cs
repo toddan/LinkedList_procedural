1d67b603266fb4f10402e81029cc23c4fd889fa0
Tord Munk <tord.munk@gmail.com>
2012-12-13 02:24:54 +0100

    First commit

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LinkedList")]
+[assembly: AssemblyTitle("LinkedList_procedural")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("LinkedList")]
+[assembly: AssemblyProduct("LinkedList_procedural")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
